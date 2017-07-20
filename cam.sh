b7112ccc4d72fa14450b90f6c20ab28be66695f3
Anatoly Cherkunov <anatoly.cherkunov@effem.com>
2017-07-20 14:40:24 +0300

    Adding latest image to web

--- cam.sh
+++ cam.sh
@@ -5,6 +5,7 @@ SLEEP_TIME=3 #in seconds
 while [ true ]; do
 
 fswebcam -r 1280x720 /usb/cam/$(date -u +"%Y%m%d%H%M%S").jpg
+fswebcam -r 1280x720 /var/www/html/latest.jpg
 sleep $SLEEP_TIME;
 
 done;
--- cam_pi.sh
+++ cam_pi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SLEEP_TIME=3 #in seconds
+SLEEP_TIME=240 #in seconds
 
 while [ true ]; do
 
 raspistill -w 1280 -h 720 -e jpg -o /usb/cam/$(date -u +"%Y%m%d%H%M%S").jpg
+raspistill -w 1280 -h 720 -e jpg -o /var/www/html/latest.jpg
 sleep $SLEEP_TIME;
 
 done;
